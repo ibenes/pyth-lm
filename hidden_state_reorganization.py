306f69ec515e1679ec64e78e07a2dbbd408b2744
Benes Karel <ibenes@fit.vutbr.cz>
2018-01-23 09:10:15 +0100

    hidden_state_reorganization: support SRNs (whose h is just a Tensor)

--- hidden_state_reorganization.py
+++ hidden_state_reorganization.py
@@ -11,11 +11,23 @@ class HiddenStateReorganizer():
 
         if mask.size(0) > batch_size:
             raise ValueError("Cannot reorganize mask {} to batch size {}".format(mask, batch_size))
-        reorg = [h[:,mask] for h in last_h]
 
-        if mask.size(0) < batch_size:
-            nb_needed_h0 = batch_size - mask.size(0)
-            additional_h = self._h0_provider.init_hidden(nb_needed_h0)
-            reorg = [torch.cat([r_h, a_h], dim=1) for r_h, a_h in zip(reorg, additional_h)]
+        if isinstance(last_h, tuple):
+            reorg = [h[:,mask] for h in last_h]
+            if mask.size(0) < batch_size:
+                nb_needed_h0 = batch_size - mask.size(0)
+                additional_h = self._h0_provider.init_hidden(nb_needed_h0)
+                reorg = [torch.cat([r_h, a_h], dim=1) for r_h, a_h in zip(reorg, additional_h)]
+            return tuple(reorg)
 
-        return tuple(reorg)
+        elif isinstance(last_h, torch._TensorBase):
+            reorg = last_h[:,mask]
+            if mask.size(0) < batch_size:
+                nb_needed_h0 = batch_size - mask.size(0)
+                additional_h = self._h0_provider.init_hidden(nb_needed_h0)
+                reorg = torch.cat([reorg, additional_h], dim=1)
+            return reorg
+
+        else:
+            raise TypeError("last_h has unsupported type {}, only tuples and tensors are accepted",
+                        last_h.__class__)
--- test/test_hidden_state_reoganization.py
+++ test/test_hidden_state_reoganization.py
@@ -13,6 +13,8 @@ class Dummy_lstm():
             torch.FloatTensor([[[0.0] * self._nb_hidden] * batch_size])
         )
 
+
+
 class HiddenStateReorganizerTests(unittest.TestCase):
     def setUp(self):
         lm = Dummy_lstm(nb_hidden=2)
@@ -108,4 +110,76 @@ class HiddenStateReorganizerTests(unittest.TestCase):
             torch.FloatTensor([[[0.2, 0.2], [0.3, 0.3], [0.0, 0.0]]]),
             torch.FloatTensor([[[2.0, 2.0], [3.0, 3.0], [0.0, 0.0]]]),
         )
+        self.hidden_equal(new_h, expected)
+
+
+class Dummy_srn():
+    def __init__(self, nb_hidden):
+        self._nb_hidden = nb_hidden
+        self._nb_layers = 1
+
+    def init_hidden(self, batch_size):
+        return torch.FloatTensor(self._nb_layers, batch_size, self._nb_hidden).zero_()
+
+
+class HiddenStateReorganizerTests_SRN(unittest.TestCase):
+    def setUp(self):
+        lm = Dummy_srn(nb_hidden=2)
+        self.reorganizer = HiddenStateReorganizer(lm)
+
+    def hidden_equal(self, actual, expected, print_them=False):
+        if print_them:
+            print("Actual:", actual)
+            print("Expected:", expected)
+
+        self.assertEqual(len(actual), len(expected))
+        for a, e in zip(actual, expected):
+            self.assertTrue(torch.equal(a, e))
+
+    def test_passing(self):
+        last_h = torch.FloatTensor([[[0.1, 0.1], [0.2, 0.2], [0.3, 0.3]]])
+
+        mask = torch.LongTensor([0, 1, 2])
+        bsz = 3
+
+        new_h = self.reorganizer(last_h, mask, bsz)
+        self.hidden_equal(new_h, last_h)
+
+    def test_shrinks(self):
+        last_h = torch.FloatTensor([[[0.1, 0.1], [0.2, 0.2], [0.3, 0.3]]])
+
+        mask = torch.LongTensor([0, 2])
+        bsz = 2
+
+        new_h = self.reorganizer(last_h, mask, bsz)
+        expected = torch.FloatTensor([[[0.1, 0.1], [0.3, 0.3]]])
+        
+        self.hidden_equal(new_h, expected, True)
+
+    def test_requires_bsz_greater_than_mask(self):
+        last_h = torch.FloatTensor([[[0.1, 0.1], [0.2, 0.2], [0.3, 0.3]]])
+
+        mask = torch.LongTensor([0, 1, 2])
+        bsz = 2
+
+        self.assertRaises(ValueError, self.reorganizer, last_h, mask, bsz)
+
+    def test_on_empty_mask_zeros(self):
+        last_h = torch.FloatTensor([[[0.1, 0.1], [0.2, 0.2], [0.3, 0.3]]])
+
+        mask = torch.LongTensor([])
+        bsz = 2
+
+        new_h = self.reorganizer(last_h, mask, bsz)
+        expected = torch.FloatTensor([[[0.0, 0.0], [0.0, 0.0]]])
+        self.hidden_equal(new_h, expected)
+
+    def test_completion_by_zeros(self):
+        last_h = torch.FloatTensor([[[0.1, 0.1], [0.2, 0.2], [0.3, 0.3]]])
+
+        mask = torch.LongTensor([1])
+        bsz = 2
+
+        new_h = self.reorganizer(last_h, mask, bsz)
+        expected = torch.FloatTensor([[[0.2, 0.2], [0.0, 0.0]]])
         self.hidden_equal(new_h, expected)
