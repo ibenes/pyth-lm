a4b26168909bed0b08543479832782431891e47f
Benes Karel <ibenes@fit.vutbr.cz>
2018-02-05 16:09:03 +0100

    implement categorical cross-entropy

--- analysis.py
+++ analysis.py
@@ -1,5 +1,6 @@
 import torch
 
+
 def categorical_entropy(p, eps=1e-100):
     non_zeros = p > eps
 
@@ -7,3 +8,10 @@ def categorical_entropy(p, eps=1e-100):
     H_p = - torch.sum(p*log_p[non_zeros])
     return H_p / torch.log(torch.FloatTensor([2]))
     
+
+def categorical_cross_entropy(p, q, eps=1e-100):
+    non_zeros = p > eps
+
+    log_q = q.log()
+    Xent = - torch.sum((p*log_q)[non_zeros])
+    return Xent / torch.log(torch.FloatTensor([2]))
--- test/test_analysis.py
+++ test/test_analysis.py
@@ -1,7 +1,7 @@
 import unittest
 import torch 
 
-from analysis import categorical_entropy
+from analysis import categorical_entropy, categorical_cross_entropy
 
 
 class CategoricalEntropyTests(unittest.TestCase):
@@ -33,3 +33,40 @@ class CategoricalEntropyTests(unittest.TestCase):
         H_x_hat = categorical_entropy(p_x)
 
         self.assertTrue(torch.equal(H_x_hat, H_x))
+
+
+class CategoricalCrossEntropyTests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_simple(self):
+        p_x = torch.FloatTensor([0.5, 0.5])
+        q_x = torch.FloatTensor([0.25, 0.75])
+
+        xent = categorical_cross_entropy(p_x, q_x)
+
+        self.assertAlmostEqual(xent[0], 1.207518749639422, delta=1e-7)
+
+    def test_sparse_true_dist(self):
+        p_x = torch.FloatTensor([1, 0.0])
+        q_x = torch.FloatTensor([0.25, 0.75])
+
+        xent = categorical_cross_entropy(p_x, q_x)
+
+        self.assertEqual(xent[0], 2)
+
+    def test_sparse_both_same(self):
+        p_x = torch.FloatTensor([0.5, 0.5, 0.0])
+        q_x = torch.FloatTensor([0.5, 0.5, 0.0])
+
+        xent = categorical_cross_entropy(p_x, q_x)
+
+        self.assertEqual(xent[0], 1)
+
+    def test_sparse_both_different(self):
+        p_x = torch.FloatTensor([0.5, 0.0, 0.5])
+        q_x = torch.FloatTensor([0.5, 0.5, 0.0])
+
+        xent = categorical_cross_entropy(p_x, q_x)
+
+        self.assertEqual(xent[0], float("inf"))
