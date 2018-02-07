04e4473a689089d6c1fb6b7bfa6aa0b348b1a117
Benes Karel <ibenes@fit.vutbr.cz>
2018-02-07 13:14:06 +0100

    fix whitespace, remove print

--- test/test_analysis.py
+++ test/test_analysis.py
@@ -8,11 +8,13 @@ class CategoricalEntropyTests(unittest.TestCase):
     def setUp(self):
         pass
 
+
     def test_uniform_2d(self):
         p_x = torch.FloatTensor([0.5, 0.5])
         H_x = torch.FloatTensor([1.0])
 
         self.assertTrue(torch.equal(categorical_entropy(p_x), H_x))
+
 
     def test_uniform_4d(self):
         p_x = torch.FloatTensor([0.25, 0.25, 0.25, 0.25])
@@ -20,11 +22,13 @@ class CategoricalEntropyTests(unittest.TestCase):
 
         self.assertTrue(torch.equal(categorical_entropy(p_x), H_x))
 
+
     def test_nonuniform_3d(self):
         p_x = torch.FloatTensor([0.5, 0.25, 0.25])
         H_x = torch.FloatTensor([1.5])
 
         self.assertTrue(torch.equal(categorical_entropy(p_x), H_x))
+
 
     def test_sparse_2d(self):
         p_x = torch.FloatTensor([0.0, 1.0])
@@ -34,20 +38,20 @@ class CategoricalEntropyTests(unittest.TestCase):
 
         self.assertTrue(torch.equal(H_x_hat, H_x))
 
+
     def test_uniform_2_dists(self):
         p_x = torch.FloatTensor([[0.5, 0.5], [0.0, 1.0]])
         H_x = torch.FloatTensor([1.0, 0.0])
 
         H_x_hat = categorical_entropy(p_x) 
-        print(H_x_hat)
 
         self.assertTrue(torch.equal(H_x_hat, H_x))
-
 
 
 class CategoricalCrossEntropyTests(unittest.TestCase):
     def setUp(self):
         pass
+
 
     def test_simple(self):
         p_x = torch.FloatTensor([0.5, 0.5])
@@ -57,6 +61,7 @@ class CategoricalCrossEntropyTests(unittest.TestCase):
 
         self.assertAlmostEqual(xent[0], 1.207518749639422, delta=1e-7)
 
+
     def test_sparse_true_dist(self):
         p_x = torch.FloatTensor([1, 0.0])
         q_x = torch.FloatTensor([0.25, 0.75])
@@ -64,6 +69,7 @@ class CategoricalCrossEntropyTests(unittest.TestCase):
         xent = categorical_cross_entropy(p_x, q_x)
 
         self.assertEqual(xent[0], 2)
+
 
     def test_sparse_both_same(self):
         p_x = torch.FloatTensor([0.5, 0.5, 0.0])
@@ -73,6 +79,7 @@ class CategoricalCrossEntropyTests(unittest.TestCase):
 
         self.assertEqual(xent[0], 1)
 
+
     def test_sparse_both_different(self):
         p_x = torch.FloatTensor([0.5, 0.0, 0.5])
         q_x = torch.FloatTensor([0.5, 0.5, 0.0])
@@ -80,6 +87,7 @@ class CategoricalCrossEntropyTests(unittest.TestCase):
         xent = categorical_cross_entropy(p_x, q_x)
 
         self.assertEqual(xent[0], float("inf"))
+
 
     def test_2_dists(self):
         p_x = torch.FloatTensor([[0.5, 0.5], [1.0, 0.0]])
@@ -95,6 +103,7 @@ class CategoricalKLDTests(unittest.TestCase):
     def setUp(self):
         pass
 
+
     def test_same(self):
         p_x = torch.FloatTensor([0.5, 0.5])
         q_x = torch.FloatTensor([0.5, 0.5])
@@ -102,6 +111,7 @@ class CategoricalKLDTests(unittest.TestCase):
         kld = categorical_kld(p_x, q_x)
 
         self.assertAlmostEqual(kld[0], 0.0)
+
 
     def test_simple_different(self):
         p_x = torch.FloatTensor([0.5, 0.5])
@@ -111,6 +121,7 @@ class CategoricalKLDTests(unittest.TestCase):
 
         self.assertAlmostEqual(kld[0], 0.207518749639422, delta=1e-7)
 
+
     def test_true_has_zero(self):
         p_x = torch.FloatTensor([1.0, 0.0])
         q_x = torch.FloatTensor([0.25, 0.75])
@@ -119,6 +130,7 @@ class CategoricalKLDTests(unittest.TestCase):
 
         self.assertAlmostEqual(kld[0], 2, delta=1e-7)
 
+
     def test_infinite_kld(self):
         p_x = torch.FloatTensor([0.5, 0.5])
         q_x = torch.FloatTensor([0.0, 1.0])
@@ -126,6 +138,7 @@ class CategoricalKLDTests(unittest.TestCase):
         kld = categorical_kld(p_x, q_x)
 
         self.assertEqual(kld[0], float("inf"))
+
 
     def test_2_dists(self):
         p_x = torch.FloatTensor([[0.5, 0.5], [0.5, 0.5]])
