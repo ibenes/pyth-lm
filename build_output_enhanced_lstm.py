5b5d3851dba4048e015c0e44efce415560fd3f28
Benes Karel <ibenes@fit.vutbr.cz>
2018-02-14 15:16:00 +0100

    BUGFIX: passing arguments to the constructor

--- build_output_enhanced_lstm.py
+++ build_output_enhanced_lstm.py
@@ -43,7 +43,8 @@ if __name__ == '__main__':
 
     model = smm_lstm_models.OutputEnhancedLM(
         len(vocab), args.emsize, args.nhid, 
-        args.nlayers, args.ivec_size, args.dropout, args.tied,
+        args.nlayers, args.ivec_size, args.dropout, 
+        tie_weights=args.tied,
         dropout_ivec=args.dropout_ivec
     )
 
