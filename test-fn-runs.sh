fbb80a167fd4c36d64559624865e2012b895eadf
Benes Karel <ibenes@fit.vutbr.cz>
2018-04-06 09:30:52 +0200

    remote the 'test' part

--- test-fn-runs.sh
+++ test-fn-runs.sh
@@ -9,21 +9,20 @@ python build_bengio.py \
     --unk="<unk>" \
     --emsize=20 \
     --nhid=20 \
-    --save=$EXP_DIR/$EXP_NAME.init.lm
+    --save=$EXP_DIR/$EXP_NAME.init.lm || exit 1
 
 # 1) train the FN-LM with oracle ivectors and evaluate using partial ones
 python train-ff-multifile.py \
     --train-list=$DATA_ROOT/valid-list.txt \
     --valid-list=$DATA_ROOT/test-list.txt \
-    --test-list=$DATA_ROOT/valid-list.txt \
     --concat-articles \
     --cuda \
     --load=$EXP_DIR/$EXP_NAME.init.lm \
     --save=$EXP_DIR/$EXP_NAME.lm \
-    --epochs=1
+    --epochs=1 || exit 1
 
 python eval-ff-multifile.py \
     --file-list=$DATA_ROOT/valid-list.txt \
     --concat-articles \
     --cuda \
-    --load=$EXP_DIR/$EXP_NAME.lm 
+    --load=$EXP_DIR/$EXP_NAME.lm  || exit 1
