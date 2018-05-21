27810c9b43561ea02c2f6717f778724d8ab3fd72
Benes Karel <ibenes@fit.vutbr.cz>
2018-05-21 12:23:18 +0200

    add support for character-level LM; expose nb_inputs_necessary

--- eval.py
+++ eval.py
@@ -14,14 +14,20 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='PyTorch RNN/LSTM Language Model')
     parser.add_argument('--data', type=str, required=True,
                         help='location of the data corpus')
+    parser.add_argument('--shuffle-lines', action='store_true',
+                        help='shuffle lines before every epoch')
+    parser.add_argument('--characters', action='store_true',
+                        help='work on character level, whitespace is significant')
+
     parser.add_argument('--batch_size', type=int, default=20, metavar='N',
                         help='batch size')
     parser.add_argument('--bptt', type=int, default=35,
                         help='sequence length')
+    parser.add_argument('--nb-inputs-necessary', type=int, default=1,
+                        help='number of input tokens required to obtain a single output. For most RNNs 1, for $n$-gram FF NN LM $n-1$')
+
     parser.add_argument('--seed', type=int, default=1111,
                         help='random seed')
-    parser.add_argument('--shuffle-lines', action='store_true',
-                        help='shuffle lines before every epoch')
     parser.add_argument('--cuda', action='store_true',
                         help='use CUDA')
     parser.add_argument('--load', type=str, required=True,
@@ -44,7 +50,7 @@ if __name__ == '__main__':
     print("preparing data...")
     ids = data.tokens_from_fn(args.data, lm.vocab, randomize=False)
     batched = multistream.batchify(ids, args.batch_size, args.cuda)
-    data = split_corpus_dataset.TemporalSplits(batched, 1, args.bptt)
+    data = split_corpus_dataset.TemporalSplits(batched, args.nb_inputs_necessary, args.bptt)
 
     # Run on test data.
     loss = evaluate_uniform_stream(lm.model, data)
--- train.py
+++ train.py
@@ -20,6 +20,18 @@ if __name__ == '__main__':
                         help='location of the train corpus')
     parser.add_argument('--valid', type=str, required=True,
                         help='location of the valid corpus')
+    parser.add_argument('--characters', action='store_true',
+                        help='work on character level, whitespace is significant')
+    parser.add_argument('--shuffle-lines', action='store_true',
+                        help='shuffle lines before every epoch')
+
+    parser.add_argument('--batch_size', type=int, default=20, metavar='N',
+                        help='batch size')
+    parser.add_argument('--bptt', type=int, default=35,
+                        help='sequence length')
+    parser.add_argument('--nb-inputs-necessary', type=int, default=1,
+                        help='number of input tokens required to obtain a single output. For most RNNs 1, for $n$-gram FF NN LM $n-1$')
+
     parser.add_argument('--lr', type=float, default=20,
                         help='initial learning rate')
     parser.add_argument('--beta', type=float, default=0,
@@ -28,18 +40,11 @@ if __name__ == '__main__':
                         help='gradient clipping')
     parser.add_argument('--epochs', type=int, default=40,
                         help='upper epoch limit')
-    parser.add_argument('--batch_size', type=int, default=20, metavar='N',
-                        help='batch size')
-    parser.add_argument('--bptt', type=int, default=35,
-                        help='sequence length')
+
     parser.add_argument('--seed', type=int, default=1111,
                         help='random seed')
-    parser.add_argument('--shuffle-lines', action='store_true',
-                        help='shuffle lines before every epoch')
     parser.add_argument('--cuda', action='store_true',
                         help='use CUDA')
-    parser.add_argument('--characters', action='store_true',
-                        help='work on character level, whitespace is significant')
     parser.add_argument('--log-interval', type=int, default=200, metavar='N',
                         help='report interval')
     parser.add_argument('--load', type=str, required=True,
@@ -63,14 +68,24 @@ if __name__ == '__main__':
 
     print("preparing data...")
     tokenize_regime = 'words'
+    if args.characters:
+        tokenize_regime = 'chars'
 
     train_ids = data.tokens_from_fn(args.train, lm.vocab, randomize=False, regime=tokenize_regime)
     train_batched = multistream.batchify(train_ids, args.batch_size, args.cuda)
-    train_data = split_corpus_dataset.TemporalSplits(train_batched, nb_inputs_necessary=1, nb_targets_parallel=args.bptt)
+    train_data = split_corpus_dataset.TemporalSplits(
+        train_batched,
+        nb_inputs_necessary=args.nb_inputs_necessary,
+        nb_targets_parallel=args.bptt
+    )
 
     valid_ids = data.tokens_from_fn(args.valid, lm.vocab, randomize=False, regime=tokenize_regime)
     valid_batched = multistream.batchify(valid_ids, 10, args.cuda)
-    valid_data = split_corpus_dataset.TemporalSplits(valid_batched, nb_inputs_necessary=1, nb_targets_parallel=args.bptt)
+    valid_data = split_corpus_dataset.TemporalSplits(
+        valid_batched, 
+        nb_inputs_necessary=args.nb_inputs_necessary, 
+        nb_targets_parallel=args.bptt
+    )
 
     print("training...")
     lr = args.lr
