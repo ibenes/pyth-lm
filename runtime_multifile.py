4b96fd40da0adedbab601f97d8486686768f10af
Benes Karel <ibenes@fit.vutbr.cz>
2018-05-24 10:37:31 +0200

    improve PEP8 compliance

--- runtime_multifile.py
+++ runtime_multifile.py
@@ -6,14 +6,13 @@ from runtime_utils import repackage_hidden
 
 from tensor_reorganization import TensorReorganizer
 
-from loggers import NoneLogger
 
 def prepare_inputs(inputs, batch_first, do_transpose, use_ivecs, custom_batches):
-    X = inputs[0] 
+    X = inputs[0]
     if batch_first:
         batch_size = X.size(0)
     else:
-        batch_size = X.size(1) 
+        batch_size = X.size(1)
     if do_transpose:
         X = X.t()
     X = Variable(X)
@@ -29,12 +28,12 @@ def prepare_inputs(inputs, batch_first, do_transpose, use_ivecs, custom_batches)
         ivecs = None
 
     if custom_batches:
-        mask = Variable(inputs[-1]) # 3
+        mask = Variable(inputs[-1])  # 3
     else:
         mask = None
 
     return X, targets_flat, ivecs, mask, batch_size
-    
+
 
 def evaluate_(model, data_source, use_ivecs, do_transpose, custom_batches, batch_first):
     model.eval()
@@ -50,7 +49,7 @@ def evaluate_(model, data_source, use_ivecs, do_transpose, custom_batches, batch
 
     for inputs in data_source:
         X, targets_flat, ivecs, mask, batch_size = prepare_inputs(
-            inputs, 
+            inputs,
             batch_first, do_transpose, use_ivecs, custom_batches
         )
 
@@ -72,23 +71,25 @@ def evaluate_(model, data_source, use_ivecs, do_transpose, custom_batches, batch
         total_timesteps += len(targets_flat)
 
     return total_loss[0] / total_timesteps
-    
+
 
 def evaluate(model, data_source, use_ivecs):
     return evaluate_(
-        model, data_source, 
+        model, data_source,
         use_ivecs, do_transpose=True, custom_batches=True, batch_first=True
     )
 
+
 def evaluate_no_transpose(model, data_source, use_ivecs):
     return evaluate_(
-        model, data_source, 
+        model, data_source,
         use_ivecs, do_transpose=False, custom_batches=False, batch_first=True
     )
 
+
 def evaluate_uniform_stream(model, data_source):
     return evaluate_(
-        model, data_source, 
+        model, data_source,
         use_ivecs=False, do_transpose=False, custom_batches=False, batch_first=False
     )
 
@@ -106,7 +107,7 @@ def train_(model, data, optim, logger, clip, use_ivecs, do_transpose, custom_bat
 
     for inputs in data:
         X, targets_flat, ivecs, mask, batch_size = prepare_inputs(
-            inputs, 
+            inputs,
             batch_first, do_transpose, use_ivecs, custom_batches
         )
 
@@ -132,53 +133,23 @@ def train_(model, data, optim, logger, clip, use_ivecs, do_transpose, custom_bat
         optim.step()
         logger.log(loss.data)
 
+
 def train(model, data, optim, logger, clip, use_ivecs):
     train_(
-        model, data, optim, logger, clip, 
+        model, data, optim, logger, clip,
         use_ivecs, do_transpose=True, custom_batches=True, batch_first=True
     )
 
+
 def train_no_transpose(model, data, optim, logger, clip, use_ivecs):
     train_(
-        model, data, optim, logger, clip, 
+        model, data, optim, logger, clip,
         use_ivecs, do_transpose=False, custom_batches=False, batch_first=True
     )
 
+
 def train_uniform_stream(model, data, logger, optim, clip):
     train_(
-        model, data, optim, logger, clip, 
+        model, data, optim, logger, clip,
         use_ivecs=False, do_transpose=False, custom_batches=False, batch_first=False
     )
-
-
-def train_debug(lm, data, optim, logger, batch_size, clip, cuda, use_ivecs=True, grad_logger=NoneLogger()):
-    model = lm.model
-    model.train()
-    hs_reorganizer = TensorReorganizer(model.init_hidden)
-    hidden = model.init_hidden(batch_size)
-
-    if cuda:
-        model.cuda()
-        hidden = tuple(h.cuda() for h in hidden)
-
-
-    for batch, (X, targets, ivecs, mask) in enumerate(data):
-        X = Variable(X.t())
-        hidden = hs_reorganizer(hidden, Variable(mask), X.size(1))
-        hidden = repackage_hidden(hidden)
-
-        criterion = nn.NLLLoss()
-        # print("[debug]", X, hidden, ivecs)
-        if use_ivecs:
-            output, hidden = model(X, hidden, Variable(ivecs))
-        else:
-            output, hidden = model(X, hidden)
-        loss = criterion(output.view(-1, len(lm.vocab)), variablilize_targets(targets))
-
-        optim.zero_grad()
-        loss.backward()
-        torch.nn.utils.clip_grad_norm(model.parameters(), clip)
-        grad_logger.log()
-
-        optim.step()
-        logger.log(loss.data)
