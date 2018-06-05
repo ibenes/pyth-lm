744867385b19e26a652cfdd471de573199b7166b
Benes Karel <ibenes@fit.vutbr.cz>
2018-06-05 18:15:40 +0200

    move test-*.sh into a separate folder

--- test-script-level/test-everything-runs.sh
+++ test-script-level/test-everything-runs.sh
@@ -5,15 +5,16 @@ WT_ROOT=/mnt/matylda5/ibenes/text-data/wikitext-2-explicit-eos/
 IVEC_EXTRACTOR=/mnt/matylda5/ibenes/projects/santosh-lm/smm-models/wt2-model/extractor-k50
 
 # note that only validation part of WT-2 is used for training to speed the test up
+TEST_DIR="${BASH_SOURCE%/*}"
 
 # 1) build a standard LSTM
-./test-lstm-runs.sh $EXP_DIR lsmt $WT_ROOT || exit 1
+$TEST_DIR/test-lstm-runs.sh $EXP_DIR lsmt $WT_ROOT || exit 1
 
 # 2) build a SMM-LSTM
-./test-smm-lstm-runs.sh $EXP_DIR smm-lstm $WT_ROOT $IVEC_EXTRACTOR || exit 1
+$TEST_DIR/test-smm-lstm-runs.sh $EXP_DIR smm-lstm $WT_ROOT $IVEC_EXTRACTOR || exit 1
 
 # 3) build iFN-LM
-./test-fn-runs.sh $EXP_DIR ifn $WT_ROOT  || exit 1
+$TEST_DIR/test-fn-runs.sh $EXP_DIR ifn $WT_ROOT  || exit 1
 
 # 4) build iFN-LM
-./test-ifn-runs.sh $EXP_DIR ifn $WT_ROOT $IVEC_EXTRACTOR || exit 1
+$TEST_DIR/test-ifn-runs.sh $EXP_DIR ifn $WT_ROOT $IVEC_EXTRACTOR || exit 1
