8db0b4efbdd1e3d83fd34fa4b88581d1a01d3054
Benes Karel <ibenes@fit.vutbr.cz>
2018-10-04 09:32:25 +0200

    Extract AlignmentExtractor as a class from align() function

--- balls/oov_alignment_lib.py
+++ balls/oov_alignment_lib.py
@@ -10,6 +10,49 @@ HORIZONAL_MOVE = 1
 VERTICAL_MOVE = -1
 
 
+class AlignmentExtractor:
+    def __init__(self):
+        pass
+
+    def extract(self, a, b, moves_taken):
+        ptr_a = moves_taken.shape[0] - 1
+        ptr_b = moves_taken.shape[1] - 1
+        alignment = []
+        words_a = []
+        words_b = []
+        while ptr_a != 0 or ptr_b != 0:
+            move = moves_taken[ptr_a, ptr_b]
+
+            if move == VERTICAL_MOVE:
+                ptr_a -= 1
+                words_a.append(a[ptr_a])
+            elif move == HORIZONAL_MOVE:
+                ptr_b -= 1
+                words_b.append(b[ptr_b])
+            else:
+                if ptr_a >= 1:
+                    words_a.append(a[ptr_a-1])
+                if ptr_b >= 1:
+                    words_b.append(b[ptr_b-1])
+
+                ptr_a -= 1
+                ptr_b -= 1
+
+                if ptr_a == 0 or ptr_b == 0:
+                    continue # no flushing
+
+                alignment.append((list(reversed(words_a)), list(reversed(words_b))))
+                words_a = []
+                words_b = []
+
+        # one final flush
+        alignment.append((list(reversed(words_a)), list(reversed(words_b))))
+        words_a = []
+        words_b = []
+
+        return list(reversed(alignment))
+
+
 def align(a, b):
     local_costs = np.zeros(shape=(len(a)+1, len(b)+1))
 
@@ -46,45 +89,9 @@ def align(a, b):
                 else:
                     moves_taken[i, j] = DIAGONAL_MOVE
 
-    ptr_a = moves_taken.shape[0] - 1
-    ptr_b = moves_taken.shape[1] - 1
-    alignment = []
-    words_a = []
-    words_b = []
-    while ptr_a != 0 or ptr_b != 0:
-        move = moves_taken[ptr_a, ptr_b]
-
-        if move == VERTICAL_MOVE:
-            ptr_a -= 1
-            words_a.append(a[ptr_a])
-        elif move == HORIZONAL_MOVE:
-            ptr_b -= 1
-            words_b.append(b[ptr_b])
-        else:
-            if ptr_a >= 1:
-                words_a.append(a[ptr_a-1])
-            if ptr_b >= 1:
-                words_b.append(b[ptr_b-1])
-
-            ptr_a -= 1
-            ptr_b -= 1
-
-            if ptr_a == 0 or ptr_b == 0:
-                continue # no flushing
-
-            alignment.append((list(reversed(words_a)), list(reversed(words_b))))
-            words_a = []
-            words_b = []
-
-    # one final flush
-    alignment.append((list(reversed(words_a)), list(reversed(words_b))))
-    words_a = []
-    words_b = []
-
-    alignment = list(reversed(alignment))
     # assert(alignment[0] == ([], []))
     # alignment = alignment[1:]
 
-    print(partial_costs)
-    print(moves_taken)
-    return alignment
+    # print(partial_costs)
+    # print(moves_taken)
+    return AlignmentExtractor().extract(a, b, moves_taken)
