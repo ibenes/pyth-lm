c24628946bb0d1adaee2bbd227f5b5e030c2644a
Benes Karel <ibenes@fit.vutbr.cz>
2018-10-04 09:04:50 +0200

    Start implementation of word-level alignment based reference extraction.
    	new file:   balls/oov_alignment_lib.py
    	new file:   balls/scripts/oov-reference-from-words.py

---
+++ balls/oov_alignment_lib.py
@@ -0,0 +1,80 @@
+import numpy as np
+
+
+def word_distance(a, b):
+    return 0 if a == b else 1
+
+
+DIAGONAL_MOVE = 0
+HORIZONAL_MOVE = 1
+VERTICAL_MOVE = -1
+
+
+def align(a, b):
+    local_costs = np.zeros(shape=(len(a)+1, len(b)+1))
+
+    for i, w_a in enumerate(a):
+        for j, w_b in enumerate(b):
+            local_costs[i+1, j+1] = word_distance(w_a, w_b)
+
+    partial_costs = np.full(shape=(len(a)+1, len(b)+1), fill_value=np.inf)
+    moves_taken = np.full(shape=(len(a)+1, len(b)+1), fill_value=np.inf)
+
+    for i in range(partial_costs.shape[0]):
+        for j in range(partial_costs.shape[1]):
+            if i == 0 and j == 0:
+                partial_costs[i, j] = 0.0
+                moves_taken[i, j] = DIAGONAL_MOVE
+            elif i == 0 and j != 0:
+                partial_costs[i, j] = partial_costs[i, j-1] + 1.0
+                moves_taken[i, j] = HORIZONAL_MOVE
+            elif i != 0 and j == 0:
+                partial_costs[i, j] = partial_costs[i-1, j] + 1.0
+                moves_taken[i, j] = VERTICAL_MOVE
+            else:
+                vertical_cost = partial_costs[i-1, j] + 1.0
+                horizontal_cost = partial_costs[i, j-1] + 1.0
+                diagonal_cost = partial_costs[i-1, j-1] + local_costs[i, j]
+
+                best_cost = min([vertical_cost, horizontal_cost, diagonal_cost])
+
+                partial_costs[i, j] = best_cost
+                if best_cost == vertical_cost:
+                    moves_taken[i, j] = VERTICAL_MOVE
+                elif best_cost == horizontal_cost:
+                    moves_taken[i, j] = HORIZONAL_MOVE
+                else:
+                    moves_taken[i, j] = DIAGONAL_MOVE
+
+    ptr_a = moves_taken.shape[0] - 1
+    ptr_b = moves_taken.shape[1] - 1
+    alignment = []
+    words_a = []
+    words_b = []
+    while ptr_a != 0 or ptr_b != 0:
+        move = moves_taken[ptr_a, ptr_b]
+
+        if move == VERTICAL_MOVE:
+            ptr_a -= 1
+            words_a.append(a[ptr_a-1])
+        elif move == HORIZONAL_MOVE:
+            ptr_b -= 1
+            words_b.append(b[ptr_b-1])
+        else:
+            ptr_a -= 1
+            ptr_b -= 1
+
+            if ptr_a >= 1:
+                words_a.append(a[ptr_a-1])
+            if ptr_b >= 1:
+                words_b.append(b[ptr_b-1])
+
+            alignment.append((list(reversed(words_a)), list(reversed(words_b))))
+            words_a = []
+            words_b = []
+
+    alignment = list(reversed(alignment))
+    assert(alignment[0] == ([], []))
+    alignment = alignment[1:]
+
+    return partial_costs, moves_taken, alignment
---
+++ balls/scripts/oov-reference-from-words.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import argparse
+import sys
+
+import numpy as np
+
+from oov_alignment_lib import align
+
+
+def parse_oov_id(oov_id):
+    return tuple(oov_id.split('_'))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--text-references', required=True)
+    args = parser.parse_args()
+
+    np.set_printoptions(threshold=2000, linewidth=np.inf)
+
+    references = {}
+    with open(args.text_references) as f:
+        for line in f:
+            fields = line.split()
+            references[fields[0]] = fields[1:]
+
+    for line in sys.stdin:
+        fields = line.split()
+        _, utt_id, _, _, _ = parse_oov_id(fields[0])
+
+        candidate_line = fields[1:]
+        reference_line = references[utt_id]
+        costs, moves, alignment = align(reference_line, candidate_line)
+
+        print('------' * 10)
+        print(candidate_line)
+        print(reference_line)
+        print(costs)
+        print(moves)
+        print(alignment)
