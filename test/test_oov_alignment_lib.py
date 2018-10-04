ebd86aa8de9944f25c249f1dc088ebdab2f63530
Benes Karel <ibenes@fit.vutbr.cz>
2018-10-04 10:18:53 +0200

    Add tests for multiple end deletions/insertions.
    	modified:   test/test_oov_alignment_lib.py

--- test/test_oov_alignment_lib.py
+++ test/test_oov_alignment_lib.py
@@ -45,12 +45,32 @@ class AlignTest(TestCase):
 
         self.assertEqual(align(a, b), expected)
 
+    def test_double_insertion(self):
+        a = "a".split()
+        b = "a b c".split()
+
+        expected = [
+            (['a'], ['a', 'b', 'c']),
+        ]
+
+        self.assertEqual(align(a, b), expected)
+
     def test_single_insertion_reversed(self):
         a = "b".split()
         b = "a b".split()
 
         expected = [
             (['b'], ['a', 'b']),
+        ]
+
+        self.assertEqual(align(a, b), expected)
+
+    def test_double_insertion_reversed(self):
+        a = "b".split()
+        b = "c a b".split()
+
+        expected = [
+            (['b'], ['c', 'a', 'b']),
         ]
 
         self.assertEqual(align(a, b), expected)
@@ -65,12 +85,32 @@ class AlignTest(TestCase):
 
         self.assertEqual(align(a, b), expected)
 
+    def test_double_deletion(self):
+        a = "a b c".split()
+        b = "a".split()
+
+        expected = [
+            (['a', 'b', 'c'], ['a']),
+        ]
+
+        self.assertEqual(align(a, b), expected)
+
     def test_single_deletion_reversed(self):
         a = "a b".split()
         b = "b".split()
 
         expected = [
             (['a', 'b'], ['b']),
+        ]
+
+        self.assertEqual(align(a, b), expected)
+
+    def test_double_deletion_reversed(self):
+        a = "c a b".split()
+        b = "b".split()
+
+        expected = [
+            (['c', 'a', 'b'], ['b']),
         ]
 
         self.assertEqual(align(a, b), expected)
