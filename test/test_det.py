34563da43b7b71b1c7bfe6cf68bf9391fd92410f
Benes Karel <ibenes@fit.vutbr.cz>
2018-10-08 14:35:05 +0200

    Add a few more tests.
    	modified:   test/test_det.py

--- test/test_det.py
+++ test/test_det.py
@@ -31,3 +31,37 @@ class DetPointTests(TestCase):
         ]
 
         self.assertEqual(det_points_from_score_tg(score_tg), det_points)
+
+    def test_simple_bad_system(self):
+        score_tg = [
+            (1.0, 0),
+            (0.0, 1),
+        ]
+
+        det_points = [
+            [0.0, 0.5],
+            [0.5, 0.5],
+            [0.5, 0.0],
+        ]
+
+        self.assertEqual(det_points_from_score_tg(score_tg), det_points)
+
+    def test_multiple_points(self):
+        score_tg = [
+            (0.0, 0),
+            (0.1, 1),
+            (0.2, 0),
+            (0.3, 1),
+            (0.4, 1),
+        ]
+
+        det_points = [
+            [0.0, 0.4],
+            [0.0, 0.2],
+            [0.2, 0.2],
+            [0.2, 0.0],
+            [0.4, 0.0],
+            [0.6, 0.0],
+        ]
+
+        self.assertEqual(det_points_from_score_tg(score_tg), det_points)
