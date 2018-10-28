3fe6a1ea0371bed76c1327a48db1aad40102c87c
Karel Benes <ibenes@fit.vutbr.cz>
2018-10-28 13:27:49 +0100

    Use PyTorch 0.4 way of constructing Tensors.

--- balls/oov_clustering/embeddings_computation.py
+++ balls/oov_clustering/embeddings_computation.py
@@ -2,5 +2,4 @@ import torch
 
 
 def tensor_from_words(words, vocab):
-    tensor = torch.LongTensor([vocab[w] for w in words]).view(1, -1)
-    return torch.autograd.Variable(tensor)
+    return torch.tensor([vocab[w] for w in words]).view(1, -1)
