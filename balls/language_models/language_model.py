92ae65819bc0cdc26537828b217a06b26973ee9f
Benes Karel <ibenes@fit.vutbr.cz>
2019-06-13 10:50:52 +0200

    Implement LanguageModel.single_sentence_nll()
    Therefore users do not need to understand LM's internals

--- balls/language_models/language_model.py
+++ balls/language_models/language_model.py
@@ -10,3 +10,13 @@ class LanguageModel(torch.nn.Module):
         self.vocab = vocab
 
         self.forward = model.forward
+
+    def single_sentence_nll(self, sentence, prefix):
+        prefix_id = self.vocab[prefix]
+        sentence_ids = [self.vocab[c] for c in sentence]
+
+        device = next(self.parameters()).device
+        tensor = torch.tensor([prefix_id] + sentence_ids).view(-1, 1).to(device)
+        o, _ = self.model(tensor[:-1], self.model.init_hidden(1))
+        nll, _ = self.decoder.neg_log_prob(o, tensor[1:])
+        return nll.item()
